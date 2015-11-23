09d0b62cafc8a4f8e4983f7056f0efd813a9d9c6
Jmily <weiyi.li713@gmail.com>
2015-11-23 22:03:37 +1100

    property detail

---
+++ src/AppBundle/Controller/PropertyController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PropertyController extends Controller
+{
+
+    /**
+     * @Route("/property/{id}", name="property")
+     */
+    public function indexAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $property = $em->getRepository('AppBundle:Property')->find($id);
+
+        if ($property == null) {
+            throw $this->createNotFoundException(
+                'No Property found'
+            );
+        }
+        else
+        {
+            return $this->render('property.html.twig', array('property'=>$property));
+
+        }
+    }
+}
